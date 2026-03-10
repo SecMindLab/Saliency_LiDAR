0c91bb2cdc5f31b2d26edf17d4c9cff3ff468341
YouChange <chengzeng.you19@imperial.ac.uk>
2026-03-10 18:49:05 +0800

    license

--- second/pytorch/saliency_map.py
+++ second/pytorch/saliency_map.py
@@ -1,8 +1,18 @@
-# target network: second and pointpillars 
-# datasest: nuscenes and kitti
-# object: car, pedestrian and cyclist
-# use expanded bbox for perturbation, while use gt bbox for IOU calculation.
-# problem: gradient computation is slow. Each scene, compute 25 times.
+#    Copyright 2026 YouChange. All Rights Reserved.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+# ==============================================================================
+
 '''
 cd /vol/bitbucket/cy19/saliency_map_v2/second
 
